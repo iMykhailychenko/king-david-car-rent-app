0a746abb962471f281c68c6db0a8f744288fb58b
iMykhailychenko <igor.c.m@ukr.net>
2020-07-30 09:58:14 +0300

    mobile

--- helpers/analitic.ts
+++ helpers/analitic.ts
@@ -4,12 +4,10 @@ export const isAnalitic = (): boolean => {
     try {
         const storedHours = localStorage.getItem('hours');
 
-        if (storedHours === null || JSON.parse(storedHours) < hours - 1) {
-            const newHours = JSON.stringify(new Date().getHours());
-            localStorage.setItem('hours', newHours);
-            return true;
-        }
-        return false;
+        if (storedHours !== null && JSON.parse(storedHours) > hours - 1) return false;
+        const newHours = JSON.stringify(new Date().getHours());
+        localStorage.setItem('hours', newHours);
+        return true;
 
     } catch (err) {
         return true;
